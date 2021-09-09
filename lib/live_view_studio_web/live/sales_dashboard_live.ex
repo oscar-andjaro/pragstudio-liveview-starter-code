0b7ffe71234971e37e194ebda83f8ad82a156ec8
Òscar Marí <omari@andjaro.com>
2021-09-09 11:48:37 +0200

    add a field to select the refresh interval to the dashboard

--- lib/live_view_studio_web/live/sales_dashboard_live.ex
+++ lib/live_view_studio_web/live/sales_dashboard_live.ex
@@ -2,12 +2,19 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
   use LiveViewStudioWeb, :live_view
   alias LiveViewStudio.Sales
 
+  @refresh_options [{"1s", 1}, {"5s", 5}, {"15s", 15}, {"30s", 30}, {"60s", 60}]
+
   def mount(_params, _session, socket) do
     socket = assign_stats(socket)
+    socket = assign(socket, :interval, "1")
 
-    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
+    if connected?(socket), do: set_next_refresh("1")
 
     {:ok, socket}
+  end
+
+  defp set_next_refresh(interval) do
+    :timer.send_after(String.to_integer(interval) * 1000, self(), :tick)
   end
 
   def render(assigns) do
@@ -40,6 +47,11 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
           </span>
         </div>
       </div>
+      <form phx-change="set_interval">
+        <select name="refresh_interval">
+        <%= options_for_select(get_refresh_options(), @interval) %>
+        </select>
+      </form>
       <button phx-click="refresh">
         <img src="images/refresh.svg">
         Refresh
@@ -48,8 +60,16 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
     """
   end
 
+  defp get_refresh_options, do: @refresh_options
+
   def handle_event("refresh", _, socket) do
     socket = assign_stats(socket)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("set_interval", %{"refresh_interval" => interval}, socket) do
+    socket = assign(socket, :interval, interval)
 
     {:noreply, socket}
   end
@@ -61,6 +81,8 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
         sales_amount: Sales.sales_amount(),
         satisfaction: Sales.satisfaction()
       )
+
+    set_next_refresh(socket.assigns.interval)
 
     {:noreply, socket}
   end
